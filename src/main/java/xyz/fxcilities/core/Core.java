aa8342734cfea6e52992ee7857425c682b642fa7
Fxcilities <fxcilities@deltanetmc.xyz>
2022-03-28 19:51:22 -0400

    fuck watermarks

--- src/main/java/xyz/fxcilities/core/Core.java
+++ src/main/java/xyz/fxcilities/core/Core.java
@@ -94,8 +94,6 @@ public abstract class Core extends JavaPlugin implements Global {
             commandMap.register(command.getLabel(), command);
             console.print(true, "Registered command /" + command.getLabel());
         }
-
-        console.print(true, getPluginName() + " &fis running &c" + VERSION);
     }
 
     @Override
--- src/main/java/xyz/fxcilities/core/Global.java
+++ src/main/java/xyz/fxcilities/core/Global.java
@@ -1,8 +1,6 @@
 package xyz.fxcilities.core;
 
 public interface Global {
-    String VERSION = "FxcilCore v0.1";
-    
     String CONSOLE_LINE = "______________________________________________________________";
     String CHAT_LINE    = "&m-----------------------------------------------------";
 }
