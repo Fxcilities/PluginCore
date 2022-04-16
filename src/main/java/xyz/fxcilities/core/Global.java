c49552ae9091d3139a0bbf6aecf1d8f3115c83ac
Fxcil <48619853+Fxcilities@users.noreply.github.com>
2022-04-16 18:46:14 -0400

    Update Global.java

--- src/main/java/xyz/fxcilities/core/Global.java
+++ src/main/java/xyz/fxcilities/core/Global.java
@@ -2,5 +2,5 @@ package xyz.fxcilities.core;
 
 public interface Global {
     String CONSOLE_LINE = "______________________________________________________________";
-    String CHAT_LINE = "&m-----------------------------------------------------";
+    String CHAT_LINE    = "&m-----------------------------------------------------";
 }
